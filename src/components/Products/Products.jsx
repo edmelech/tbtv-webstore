bc4d84eaea5f937834b678499447a0b1b96ed45e
Ed Melech <mr.melech@gmail.com>
2022-01-31 15:00:57 +0000

    create an array of products

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Grid from @material-ui/ConvolverNode;
+
+const products = [
+  {id: 1, name: 'Vinyl', description: 'Deny All - Ltd edition vinyl.'}
+  {id: 2, name: 'T-shirt', description: 'TBTV LP1 Artwork T-shirt.'}
+]
+
+const Products = () => {
+  <main>
+    <Grid container justify="center" spacing={4}>
+
+    </Grid>
+  </main>
+}
+
+export default Products;
