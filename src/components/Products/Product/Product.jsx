97c3ba26ae109573ce3466b80fab5ed35e7c01eb
Ed Melech <mr.melech@gmail.com>
2022-01-31 16:36:40 +0000

    import components from material UI

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons'
+
+export const Product = () => {
+  return <div></div>;
+};
