35ede72bde7b461177e5ddb443bb4de40edbe029
Ed Melech <mr.melech@gmail.com>
2022-02-08 19:13:20 +0000

    fix errors

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
-import { mergeClasses } from '@material-ui/styles';
+
+import useStyles from './styles';
+import CartItem from './CartItem/CartItem';
 
 const Cart = ({ cart }) => {
-  const isEmpty = !cart.line_items.length; 
+  const classes = useStyles();
 
   const EmptyCart = () => (
     <Typography variant="subtitle1">You have no items in your shopping cart, start adding some!</Typography>
@@ -14,26 +16,28 @@ const Cart = ({ cart }) => {
     <Grid container spacing= {3}>
       {cart.line_items.map((item) => (
         <Grid item xs={12} sm={4} key={item.id}>
-          <div>{item.name}</div> 
+          <CartItem item={item} />
         </Grid>
       ))}
     </Grid>
     <div className={classes.cardDetails}>
         <Typography variant="h4">Subtotal: { cart.subtotal.formatted_with_symbol}</Typography>
         <div>
-          <Button className={Classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Empty Cart</Button>
-          <Button className={Classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
+          <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Empty Cart</Button>
+          <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
         </div>
     </div>
     </>
-  )
+  );
+
+  if(!cart.line_items) return 'Loading ...';
 
   return (
     <Container>
       <div className={classes.toolbar} />
       <Typography className={classes.title} variant="h3">Your Shopping Cart</Typography>
-      { isEmpty ? <EmptyCart /> : <FilledCart />}
-    </Container>;
+      { !cart.line_items.length ? <EmptyCart /> : <FilledCart /> }
+    </Container>
   )
 };
 
