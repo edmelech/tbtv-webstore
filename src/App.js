5afe60bef45e0913688ad9acb2b35612779c9a88
Ed Melech <mr.melech@gmail.com>
2022-02-10 18:31:01 +0000

    create handle update cart logic

--- src/App.js
+++ src/App.js
@@ -18,10 +18,18 @@ const App = () => {
   }
 
   const handleAddToCart = async (productId, quantity) => {
-    const item = await commerce.cart.add(productId, quantity);
+    const { cart } = await commerce.cart.add(productId, quantity);
 
-    setCart(item.cart);
+    setCart(cart);
   }
+
+  const handleUpdateCartQty = async(productId, quantity) => {
+    const { cart } = await commerce.cart.update(productId, { quantity });
+
+    setCart(cart)
+  }
+
+  const 
 
   useEffect(() => {
     fetchProducts();
