0b020e107986b481866f260f6d4ca6b8065af275
Ed Melech <mr.melech@gmail.com>
2022-03-02 16:02:01 +0000

    put missing dependencies in useEffect dependency arrays

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -45,7 +45,7 @@ const AddressForm = ({ checkoutToken, next }) => {
 
   useEffect(()=> {
     fetchShippingCountries(checkoutToken.id)
-  }, []);
+  }, [checkoutToken.id]);
 
   useEffect(() => {
    if(shippingCountry) fetchSubdivisions(shippingCountry)
@@ -53,7 +53,7 @@ const AddressForm = ({ checkoutToken, next }) => {
 
   useEffect(() => {
     if(shippingSubdivision) fetchShippingOptions(checkoutToken.id, shippingCountry, shippingSubdivision);
-  }, [shippingSubdivision])
+  }, [shippingSubdivision, checkoutToken.id, shippingCountry])
 
   return (
     <>
