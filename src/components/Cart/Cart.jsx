bc9d308ba520e368f3d03a3765b9f3e3cc4c37d7
Ed Melech <mr.melech@gmail.com>
2022-03-07 23:02:13 +0000

    change colours of buttons

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -26,9 +26,9 @@ const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart
         ))}
       </Grid>
       <div className={classes.cardDetails}>
-          <Typography variant="h4">Subtotal: { cart.subtotal.formatted_with_symbol}</Typography>
+          <Typography variant="h5">SUBTOTAL: { cart.subtotal.formatted_with_symbol}</Typography>
           <div>
-            <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart}>Empty Cart</Button>
+            <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="common" onClick={handleEmptyCart}>Empty Cart</Button>
             <Button component={Link} to="/checkout" className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
           </div>
       </div>
@@ -40,7 +40,7 @@ const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart
   return (
     <Container>
       <div className={classes.toolbar} />
-      <Typography className={classes.title} variant="h3">Your Shopping Cart</Typography>
+      <Typography className={classes.title} variant="h5" gutterBottom>YOUR SHOPPING CART</Typography>
       { !cart.line_items.length ? <EmptyCart /> : <FilledCart /> }
     </Container>
   )
